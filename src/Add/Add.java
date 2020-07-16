ab6f0c662a8c5b7b07fad37993b2024e48b808d2
MobassaraTabassum <mobassarat@gmail.com>
2020-07-16 01:06:46 -0400

    add demo file

---
+++ src/Add/Add.java
@@ -0,0 +1,16 @@
+package Add;
+
+public class Add {
+    int a =8;
+    int b=3;
+
+    public static void main(String[] args) {
+        int a =8;
+        int b=3;
+        System.out.println(a+=b);
+        System.out.println(a-=3);
+        System.out.println(a*=7);
+        System.out.println(a/=b);
+        System.out.println(a%=5);
+    }
+}
